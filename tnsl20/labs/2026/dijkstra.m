af9a10a7bb2d6783ccbc9fb53b5f2023279c3d4c
Anastasia Lemetti <anlemett@gmail.com>
2026-09-03 13:28:03 +0200

    TNSL20 2026: lab 1-2

---
+++ tnsl20/labs/2026/dijkstra.m
@@ -0,0 +1,42 @@
+clear; close all; clc;
+
+% Step 1
+% A =
+% G = 
+% Plotting
+
+% Step 2
+% starting_vertex = 
+
+% Step 3
+% n = 
+
+% Step 4
+% labels = 
+% labels(starting_vertex) =
+
+% Step 5
+% unvisited_vertices = 
+
+for i=1:n
+    % Step 6
+    % unvisited_vertices_labels = 
+    
+    % Step 7
+    % [current_label, current_idx] =
+
+    % Step 8
+    % current_vertex = 
+    
+    % Routine to update labels of vertices adjacent to current_vertex
+    for j = unvisited_vertices
+        % add your code here
+        % (see pseudocode)
+    end
+    
+    % Step 9
+    % unvisited_vertices(current_idx) =
+end
+
+% Step 10: Display the result
+% add your code here
